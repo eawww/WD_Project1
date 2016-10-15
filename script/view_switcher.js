31525216459da1c4bf63816c3d3d5118f155306b
Eric Wilson <eawww0@gmail.com>
2016-10-15 16:54:05 -0500

    list items now display; further formatting to come

--- script/view_switcher.js
+++ script/view_switcher.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-    
+    $("#grid_button").attr('src','icons/grid_pressed.jpg');
+    //initial_gridification();
+    $("#grid_button").on('click', function(){
+        $("#grid_button").attr('src','icons/grid_pressed.jpg');
+        $("#list_button").attr('src','icons/list.jpg');
+    });
+    $("#list_button").on('click', function(){
+        $("#grid_button").attr('src','icons/grid.jpg');
+        $("#list_button").attr('src','icons/list_pressed.jpg');
+        var data = movies["movies"];
+        var template = $("#list-template").html();
+        var html_maker = new htmlMaker(template);
+        var html = html_maker.getHTML(data);
+        $("#movie_display").html(html);
+    });
 });
 
+function grid_button_clicked(){
+    
+}
+
+function list_button_clicked(){
+
+}
 
