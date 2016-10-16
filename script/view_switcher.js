36da7523d61d3673fe87c03257c3b7a341dc67bc
Eric Wilson <eawww0@gmail.com>
2016-10-16 11:53:04 -0500

    fix stars on listview

--- script/view_switcher.js
+++ script/view_switcher.js
@@ -27,6 +27,24 @@ $(document).ready(function(){
         var html_maker = new htmlMaker(template);
         var html = html_maker.getHTML(data);
         $("#movie_display").html(html);
+        $(".rating-stars").each(function(){
+            if ($(this).html() === "1"){
+            $(this).html("&#9733<span style=\"color:lightgray;\">&#9734&#9734&#9734&#9734</span>");
+            }
+            else if ($(this).html() === "2"){
+                $(this).html("&#9733&#9733<span style=\"color:lightgray;\">&#9734&#9734&#9734</span>");
+            }
+            else if ($(this).html() === "3"){
+                $(this).html("&#9733&#9733&#9733<span style=\"color:lightgray;\">&#9734&#9734</span>");
+            }
+            else if ($(this).html() === "4"){
+                $(this).html("&#9733&#9733&#9733&#9733<span style=\"color:lightgray;\">&#9734</span>");
+            }
+            else if ($(this).html() === "5"){
+                $(this).html("&#9733&#9733&#9733&#9733&#9733");
+            }
+        });
+        
     });
 });
 
