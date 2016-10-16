c6338f6105a2c3c7fade7d5ece5b339776e48367
Eric Wilson <eawww0@gmail.com>
2016-10-16 11:19:05 -0500

    Finish grid layout

--- script/view_switcher.js
+++ script/view_switcher.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
     $("#grid_button").attr('src','icons/grid_pressed.jpg');
     //initial_gridification();
+    $("#grid_button").attr('src','icons/grid_pressed.jpg');
+    $("#list_button").attr('src','icons/list.jpg');
+    var data = movies["movies"];
+    var template = $("#grid-template").html();
+    var html_maker = new htmlMaker(template);
+    var html = html_maker.getHTML(data);
+    $("#movie_display").html(html);
+        
     $("#grid_button").on('click', function(){
         $("#grid_button").attr('src','icons/grid_pressed.jpg');
         $("#list_button").attr('src','icons/list.jpg');
+        var data = movies["movies"];
+        var template = $("#grid-template").html();
+        var html_maker = new htmlMaker(template);
+        var html = html_maker.getHTML(data);
+        $("#movie_display").html(html);
     });
+    
     $("#list_button").on('click', function(){
         $("#grid_button").attr('src','icons/grid.jpg');
         $("#list_button").attr('src','icons/list_pressed.jpg');
